13cb15ca40b319614f49278fd4336cba4c5d8f38
Shan Tirmizi <tirmizishahnawaz@gmail.com>
2021-02-05 16:23:12 +0000

    Photo bug fixed on the product image page

--- src/components/ProductImages.js
+++ src/components/ProductImages.js
@@ -3,16 +3,16 @@ import styled from 'styled-components'
 import './ProductImages.css'
 
 const ProductImages = ({ images = [{ url:''}] }) => {
-  const [main, setMain] = useState(images[0].url)
-  // console.log(main)
+  const [main, setMain] = useState(images[0])
+
   return (
     <div className='product__images'>
-      <img src={main} alt='main' className='product__main' />
+      <img src={main.url == '' ?  images[0].url : main.url} alt='main pic' className='product__main' />
       <div className="product__secondary">
         {
-          images.slice(0, 3).map((image, index) => {
+          images.slice(0,3).map((image, index) => {
             return (
-              <img src={image.url} alt={image.filename} key={index} onClick={() => setMain(images[index])}
+              <img src={image.url} alt={image.filename} key={image.id} onClick={() => setMain(images[index])}
                 className={`${image.url === main.url && 'active'}`} />
             )
           })
